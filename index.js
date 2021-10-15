d49013c57bd4988973d13c327309f0463dbbf6c4
MckinleyVJ <87624839+mckinleyvj@users.noreply.github.com>
2021-10-16 00:20:08 +1100

    feat: set input to editor for instructional or multi-line descriptive texts

--- index.js
+++ index.js
@@ -19,28 +19,83 @@ const promptUser = () => {
             name: 'title',
           },
           {
-            type: 'input',
-            message: 'Provide a short description of your Project.\n - What inspired the author?\n - What problems did it solve?\n',
+            type: 'editor',
+            message: 'Provide a short description of your Project.',
             name: 'description',
           },
           {
             type: 'list',
             message: 'Type of license used :',
-            choices: ['MIT', 'APACHE 2.0', 'GNU GPLv3', 'BSD 3', 'None'],
+            choices: ['MIT', 'Apache-2.0', 'GPL-3', 'BSD-3', 'None'],
             name: 'license',
           },
           {
-            type: 'input',
+            type: 'editor',
+            message: 'Contribution guidelines :',
+            name: 'contributing',
+          },
+          {
+            type: 'editor',
+            message: 'Test instructions :',
+            name: 'tests',
+          },
+          {
+            type: 'editor',
             message: 'Installation requirements :',
             name: 'installation',
+          },
+          {
+            type: 'editor',
+            message: 'Usage Information :',
+            name: 'usage',
           },
     ]);
   };
 
-  const createMarkdown = ({ name, location, github, linkedin }) =>
+  const createMarkdown = ({ user_github, user_email, title, description, license, contributing, tests, installation, usage}) =>
+  `
+  # ${title}
+  [![license](https://img.shields.io/static/v1?label=license&message=${license}&color=red)](#license)
+
+  ## Description
+  ${description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  ${installation}
+
+  ## Usage
+  ${usage}
+  
+  ## License
+
+  Copyright (c) Microsoft Corporation. All rights reserved.
+
+  Licensed under the [${license}](LICENSE.txt) license.
+  
+  ## Contributing
+  ${contributing}
+  
+  ## Tests
+  ${tests}
+  
+  ## Questions
+  ${user_github}
+  ${user_email}
+  `;
 
 
   (() => {
+    console.log("===========================================================");
+    console.log("Please proceed to answer the prompts using Markdown syntax.")
+    console.log("===========================================================");
     promptUser()
       .then((response) => fs.writeFileSync('README.md', createMarkdown(response)))
       .then(() => console.log('Successfully generated README.md'))
